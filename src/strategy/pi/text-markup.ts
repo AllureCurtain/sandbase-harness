158831512ec265b83a8c3edb1faa233be7b20f92
Yao <alluresocina@163.com>
2026-09-20 11:04:09 +0800

    fix(pi): use explicit HTML text escaping

--- src/strategy/pi/text-markup.ts
+++ src/strategy/pi/text-markup.ts
@@ -135,16 +135,16 @@ function looksLikeControlPrefix(value: string): boolean {
   return true;
 }
 
-const HTML_ESCAPE_RE = /[&<>"']/g;
-const HTML_ENTITIES: Record<string, string> = {
-  '&': '&amp;',
-  '<': '&lt;',
-  '>': '&gt;',
-  '"': '&quot;',
-  "'": '&#39;',
-};
-
 function stripControlTokens(value: string): string {
   const withoutControlTokens = value.replace(PI_CONTROL_TOKEN_RE, '');
-  return withoutControlTokens.replace(HTML_ESCAPE_RE, (character) => HTML_ENTITIES[character]);
+  return escapePiHtmlText(withoutControlTokens);
+}
+
+function escapePiHtmlText(value: string): string {
+  return value
+    .replaceAll('&', '&amp;')
+    .replaceAll('<', '&lt;')
+    .replaceAll('>', '&gt;')
+    .replaceAll('"', '&quot;')
+    .replaceAll("'", '&#39;');
 }
